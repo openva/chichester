d6af1db509a33a64f143eda82962162095552dab
Waldo Jaquith <waldo@jaquith.org>
2013-10-13 15:30:18 -0400

    Build out the rest of the scraping methods

--- class.Chichester.inc.php
+++ class.Chichester.inc.php
@@ -148,16 +148,16 @@ class Chichester
 		
 		/*
 		 * Iterate through the table rows -- each row is a single TOC entry. (A bunch of table rows
-		 * are noise, but we can avoid those easily.
+		 * are noise, but we can avoid those easily.)
 		 */
 		$i=0;
-		foreach ($dom->find('tr') as $agency)
+		foreach ($this->dom->find('tr') as $agency)
 		{
 			
 			/*
 			 * If this isn't an agency listing, then skip it.
 			 */
-			if (stristr($report->find('td', 0)->plaintext, 'Agency') === FALSE)
+			if (stristr($agency->find('td', 0)->plaintext, 'Agency') === FALSE)
 			{
 				continue;
 			}
@@ -165,23 +165,29 @@ class Chichester
 			/*
 			 * Save each field.
 			 */
-			$this->agencies->{$i}->toc_id = $report->find('td', 0)->find('a', 0);
-			$this->agencies->{$i}->??? = trim($report->find('td', 0)->plaintext);
-			$this->agencies->{$i}->title = trim($report->find('td', 1)->plaintext);
+			$this->agencies->{$i}->toc_id = str_replace('http://leg1.state.va.us/000/reg/TOC',
+				'', $agency->find('td', 0)->find('a', 0)->href);
+			$this->agencies->{$i}->number = trim(str_replace('Agency ', '', $agency->find('td', 0)->plaintext));
+			$this->agencies->{$i}->name = trim($agency->find('td', 1)->plaintext);
+			
+			/*
+			 * Determine if this agency has been abolished. If it has been, strip that flag out of
+			 * the agency title, with which it's comingled. Save that flag as its own variable.
+			 */
+			if (stristr($this->agencies->{$i}->name, '(ABOLISHED)') !== FALSE)
+			{
+				$this->agencies->{$i}->abolished = TRUE;
+				$this->agencies->{$i}->name = str_replace('(ABOLISHED)', '', $this->agencies->{$i}->name);
+				$this->agencies->{$i}->name = trim($this->agencies->{$i}->name);
+			}
+			else
+			{
+				$this->agencies->{$i}->abolished = FALSE;
+			}
+			
+			$i++;
 			
 		}
-		
-		// walk DOM
-		// /000\+reg\+TOC([0-9]+)">(^<)<\/a><\/td><td>(^<)/
-		// turn into object
-		/*
-		if (stristr($title, '(ABOLISHED)') !== FALSE)
-		{
-			$abolished = TRUE;
-			$title = str_replace('(ABOLISHED)', '', $title);
-			$title = trim($title);
-		}
-		*/
 		
 		return TRUE;
 		
@@ -190,9 +196,9 @@ class Chichester
 	/**
 	 * Fetch and structure the table of contents for a single agency.
 	 * 
-	 * @requires $this->html
-	 * @returns TRUE or FALSE
-	 * @sets TBD
+	 * @requires	$this->html
+	 * @returns		TRUE or FALSE
+	 * @sets		TBD
 	 */
 	function parse_agency()
 	{
@@ -208,6 +214,79 @@ class Chichester
 		{
 			 throw new Exception($e->getMessage());
 			 return FALSE;
+		}
+		
+		
+		/*
+		 * Fetch each table row.
+		 */
+		$i=0;
+		foreach ($this->dom->find('tr') as $section)
+		{
+			
+			/*
+			 * See if this is a table row that we actually want. We look at the first cell to
+			 * determine this, and see if it's a section, a collection of forms, or a "DIBR"—a
+			 * list of documents included by reference.
+			 */
+			$td_1 = $report->find('td', 0)->plaintext;
+			
+			/*
+			 *	Is this a section, a collection of forms, or documents included by reference? If
+			 * it's none of those things, then skip it.
+			 */
+			if (stristr($td_1, 'section') === FALSE)
+			{
+				$entry_type = 'section';
+			}
+			elseif (stristr($td_1, 'forms') === FALSE)
+			{
+				$entry_type = 'forms';
+			}
+			elseif (stristr($td_1, 'dibr') === FALSE)
+			{
+				$entry_type = 'dibr';
+			}
+			else
+			{
+				continue;
+			}
+			
+			/*
+			 * Save the type of entry that this is.
+			 */
+			$this->sections->{$i}->type = $entry_type;
+			
+			/*
+			 * Save the official URL for this section.
+			 */
+			$this->sections->{$i}->official_url = $report->find('td', 0)->find('a', 0)->href;
+			
+			/*
+			 * Save the section number (e.g., "2VAC5-585-2500"). We extract this from the URL.
+			 */
+			$this->sections->{$i}->section_number = str_replace('http://leg1.state.va.us/cgi-bin/legp504.exe?000+reg+',
+				'', $this->sections->{$i}->official_url);
+			
+			/*
+			 * Save the section catch line.
+			 */
+			$this->sections->{$i}->catch_line = trim($report->find('td', 1)->plaintext);
+			
+			/*
+			 * If the catch line is "[Repealed]" then we actually have no catch line, but instead
+			 * a nameless, repealed section.
+			 */
+			if (stristr('[Repealed]', '', $this->sections->{$i}->catch_line) !== FALSE)
+			{
+				unset($this->sections->{$i}->catch_line);
+				$this->sections->{$i}->repealed = TRUE;
+			}
+			else
+			{
+				$this->sections->{$i}->repealed = FALSE;
+			}
+			
 		}
 		
 		return TRUE;
@@ -238,9 +317,82 @@ class Chichester
 			 return FALSE;
 		}
 		
+		/*
+		 * 
+		 */
+		$this->dom->find('p.part');
+		
+		/*
+		 * Save the section number and the catch line.
+		 *
+		 * For example:
+		 * <p class=vacno>16VAC30-12-40. Information to be sent to persons on the list.</p>
+		 */
+		$tmp = $this->dom->find('p.vacno');
+		$pos = strpos($tmp, '. ');
+		$this->section->section_number = substr($tmp, 0, $pos);
+		$this->section->catch_line = substr($tmp, $pos);
+		
+		/*
+		 * Save the statutory authority under which this regulation was established. This may be
+		 * multiple paragraphs, so we build it up with a loop.
+		 */
+		$this->section->authority = '';
+		foreach ($this->dom->find('p.auth')->innertext as $auth)
+		{
+			
+			/*
+			 * Skip the section header (which is just a P tag).
+			 */
+			if ($auth == 'Statutory Authority')
+			{
+				continue;
+			}
+			$this->section->authority .= $auth;
+			
+		}
+		
+		/*
+		 * Save the actual text of this regulation. We do this by getting a list of all paragraphs
+		 * with a class that starts with "sect," since we know that sections of text can have the
+		 * class "sectind" or "sectbi", which means it's possible that othe classes are lurking out
+		 * there somewhere.
+		 */
+		$this->section->text = '';
+		foreach ($this->dom->find('p[class^=sect]')->innertext as $text)
+		{
+			
+			$this->section->text .= $text;
+		}
+		
+		/*
+		 * Take our unstructured text and give it structure.
+		 */
+		$structurer = new SubsectionIdentifier();
+		$structurer->text = $this->section->text;
+		$structurer->parse();
+		$this->section->text = $structurer->structured;
+		
+		/*
+		 * Save the history of the establishment of and modifications to this regulation.
+		 */ 
+		$this->section->history = '';
+		foreach ($this->dom->find('p[class^=sect]')->innertext as $history)
+		{
+			
+			/*
+			 * Skip the section header (which is just a P tag).
+			 */
+			if ($history == 'Historical Notes')
+			{
+				continue;
+			}
+			$this->section->history .= $history;
+		}
+		
+		
 		return TRUE;
 		
 	}
-
 
 }
