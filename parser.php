b139420f0b7ffcf5b7e7b9e2d44939bc9acea812
Waldo Jaquith <waldo@jaquith.org>
2013-10-16 22:11:49 -0400

    Improve comments

--- parser.php
+++ parser.php
@@ -35,11 +35,17 @@ $chichester = new Chichester();
 $chichester->parse_toc();
 file_put_contents('output/agencies.json', json_encode($chichester->agencies));
 
+/*
+ * Iterate through the agency list and retrieve each agency's TOC (section list).
+ */
 foreach ($chichester->agencies as $agency)
 {
 	
 	$chichester->agency_id = $agency->toc_id;
 	
+	/*
+	 * Retrieve the list of sections for this agency.
+	 */
 	try
 	{
 		$chichester->parse_agency();
@@ -51,6 +57,10 @@ foreach ($chichester->agencies as $agency)
 	
 	//echo '<pre>' . print_r($chichester->sections) . '</pre>';
 	file_put_contents('output/agency-' . $agency->toc_id . '.json', json_encode($chichester->sections));
+	/*
+	 * Now iterate through each section in this agency.
+	 */
+		// THIS IS A MISTAKE. Ultimately, we even want to save repealed and remove sections.
 			file_put_contents('output/sections/' . $chichester->section->section_number . '.json',
 				json_encode($chichester->section));
 
